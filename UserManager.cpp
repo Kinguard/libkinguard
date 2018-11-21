b2c5f66e51f9c3b566afc5ca5f02a7d147e985da
Tor Krill <tor@openproducts.se>
2018-11-21 10:42:26 +0100

    More work on usermanager
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- UserManager.cpp
+++ UserManager.cpp
@@ -1,9 +1,11 @@
 #include "UserManager.h"
 #include "MailManager.h"
+#include "IdentityManager.h"
 
 #include <algorithm>
 
 #include <libutils/FileUtils.h>
+#include <libopi/JsonHelper.h>
 #include <libutils/Logger.h>
 #include <libutils/Process.h>
 #include <libopi/SysConfig.h>
@@ -27,11 +29,9 @@ UserManager::UserManager(SecopPtr authdb):authdb(authdb)
 	}
 }
 
-KGP::UserManager &KGP::UserManager::Instance()
+UserManagerPtr KGP::UserManager::Instance(SecopPtr authdb)
 {
-	static UserManager um;
-
-	return um;
+	return UserManagerPtr(new UserManager(authdb) );
 }
 
 bool UserManager::AddUser(const string &username, const string &password, const string &displayname, bool isAdmin)
@@ -66,17 +66,75 @@ bool UserManager::AddUser(const string &username, const string &password, const 
 			this->global_error = mmgr.StrError();
 			return false;
 		}
+	}
 
-		if( !mmgr.Synchronize()	)
+	// Add default email
+	IdentityManager &idmgr = IdentityManager::Instance();
+
+	if( idmgr.HasDnsProvider() )
+	{
+		string host, domain;
+		tie(host, domain) = idmgr.GetCurrentDnsName();
+		if( host!="" && domain != "" )
 		{
-			this->global_error = mmgr.StrError();
-			return false;
+			mmgr.SetAddress(host+"."+domain, username, username);
 		}
+	}
 
+	if( !mmgr.Synchronize()	)
+	{
+		this->global_error = mmgr.StrError();
+		return false;
 	}
 
 	return true;
+}
 
+bool UserManager::AddUser(const UserPtr user, const string &password, bool isAdmin)
+{
+	if( user == nullptr )
+	{
+		return false;
+	}
+
+	return this->AddUser( user->GetUsername(), password, user->GetDisplayname(), isAdmin);
+}
+
+UserPtr UserManager::GetUser(const string &username)
+{
+	vector<string> users = this->authdb->GetUsers();
+
+	if( std::find(users.begin(), users.end(), username) == users.end() )
+	{
+		this->global_error = "User not found";
+		return nullptr;
+	}
+
+	string displayname;
+	try {
+		displayname = this->authdb->GetAttribute(username, "displayname");
+	} catch (std::runtime_error& err) {
+		logg << Logger::Info << "Missing displayname for "<< username << " ("<< err.what()<<")"<<lend;
+	}
+
+	return UserPtr(new User(username, displayname) );
+}
+
+bool UserManager::UpdateUser(const UserPtr user)
+{
+	if( user == nullptr || user->GetUsername() == "" )
+	{
+		this->global_error = "Missing user object or username";
+		return false;
+	}
+
+	if( ! this->authdb->AddAttribute( user->GetUsername(), "displayname", user->GetDisplayname() ) )
+	{
+		this->global_error = "Failed to update user";
+		return false;
+	}
+
+	return true;
 }
 
 bool UserManager::DeleteUser(const string &user)
@@ -118,7 +176,204 @@ bool UserManager::DeleteUser(const string &user)
 	return true;
 }
 
+bool UserManager::DeleteUser(const UserPtr user)
+{
+	if( user == nullptr || user->GetUsername() == "")
+	{
+		this->global_error = "No user object provided or empty user";
+		return false;
+	}
+
+	return this->DeleteUser( user->GetUsername() );
+}
+
+bool UserManager::UpdateUserPassword(const string &user, const string &new_pass, const string &old_pass)
+{
+	list<map<string,string>>  ids = this->authdb->GetIdentifiers( user, "opiuser");
+	if(ids.size() == 0 )
+	{
+		this->global_error = "Database error";
+		return false;
+	}
+
+	map<string,string> id = ids.front();
+	if( id.find("password") == id.end() )
+	{
+		this->global_error = "Database error";
+		return false;
+	}
+
+	if( old_pass != "" )
+	{
+		// We should check old password against db
+		if( old_pass != id["password"] )
+		{
+			this->global_error = "Bad request";
+			return false;
+		}
+	}
+
+	if( ! this->authdb->UpdateUserPassword( user, new_pass) )
+	{
+		this->global_error = "Operation failed";
+		return false;
+	}
+
+	return true;
+}
+
+list<UserPtr> UserManager::GetUsers()
+{
+	list<UserPtr> users;
+
+	vector<string> usernames = this->authdb->GetUsers();
+	for(const auto& user: usernames)
+	{
+		users.push_back(this->GetUser( user ) );
+	}
+
+	return users;
+}
+
+list<string> UserManager::GetGroups()
+{
+	vector<string> groups = this->authdb->GetGroups();
+
+	return list<string>(groups.begin(), groups.end());
+}
+
+list<string> UserManager::GetUserGroups(const string &user)
+{
+	vector<string> groups = this->authdb->GetUserGroups( user );
+
+	return list<string>(groups.begin(), groups.end());
+}
+
+bool UserManager::AddGroup(const string &groupname)
+{
+	if( ! this->authdb->AddGroup( groupname) )
+	{
+		this->global_error = "Failed to add new group";
+		return false;
+	}
+	return true;
+}
+
+bool UserManager::DeleteGroup(const string &groupname)
+{
+	if( ! this->authdb->RemoveGroup( groupname) )
+	{
+		this->global_error = "Failed to delete group";
+		return false;
+	}
+	return true;
+}
+
+bool UserManager::AddGroupMember(const string &group, const string &member)
+{
+	if( ! this->authdb->AddGroupMember( group, member) )
+	{
+		this->global_error = "Failed to add member to group";
+		return false;
+	}
+	return true;
+}
+
+bool UserManager::DeleteGroupMembar(const string &group, const string &member)
+{
+	if( ! this->authdb->RemoveGroupMember(group, member) )
+	{
+		this->global_error = "Failed to remove member from group";
+		return false;
+	}
+
+	if( group == "admin" )
+	{
+		// User left admin role, remove from admin mail as well
+		MailManager& mmgr = MailManager::Instance();
+
+		if( ! mmgr.RemoveFromAdmin( member ) )
+		{
+			this->global_error ="Failed to remove user from admin mail";
+			return false;
+		}
+	}
+	return true;
+}
+
+list<string> UserManager::GetGroupMembers(const string &group)
+{
+	vector<string> members = this->authdb->GetGroupMembers(group);
+	return list<string>(members.begin(), members.end());
+}
+
 UserManager::~UserManager()
+{
+
+}
+
+/*
+ *
+ *  User implementation
+ *
+ */
+
+User::User(const string &username, const string &displayname): username(username), displayname(displayname)
+{
+
+}
+
+User::User(const Json::Value &userdata)
+{
+	JsonHelper::TypeChecker tc(	{
+									{0x01, "username", JsonHelper::TypeChecker::STRING},
+									{0x02, "displayname", JsonHelper::TypeChecker::STRING}
+								} );
+
+	if( ! tc.Verify( 0x01|0x02, userdata ) )
+	{
+		throw std::runtime_error("Missing parameters in user constructor");
+	}
+
+	this->username = userdata["username"].asString();
+	this->displayname = userdata["displayname"].asString();
+}
+
+string User::GetUsername()
+{
+	return this->username;
+}
+
+string User::GetDisplayname()
+{
+	return this->displayname;
+}
+
+void User::AddAttribute(const string &attr, const string& value)
+{
+	this->attributes[attr] = value;
+}
+
+string User::GetAttribute(const string &attr)
+{
+	if( this->attributes.find(attr) == this->attributes.end() )
+	{
+		throw std::runtime_error("Unable to find attribute");
+	}
+	return this->attributes[attr];
+}
+
+Json::Value User::ToJson()
+{
+	Json::Value ret;
+
+	ret["username"] = this->username;
+	ret["displayname"] = this->displayname;
+
+	return ret;
+}
+
+User::~User()
 {
 
 }
--- UserManager.h
+++ UserManager.h
@@ -5,12 +5,95 @@
 
 #include <libopi/Secop.h>
 
+#include <json/json.h>
+
+#include <memory>
 #include <string>
+#include <map>
 
 using namespace std;
 
 namespace KGP {
 
+class UserManager;
+typedef shared_ptr<UserManager> UserManagerPtr;
+
+/**
+ * @brief The User class
+ *
+ * Holds user attributes about a user
+ *
+ * Currently supports
+ * UserName - non updateable attribute that uniquely identifies user in system
+ * DisplayName - User readable version of name
+ *
+ * Custom attributes except noted above not yet supported by backend.
+ *
+ * Note that user password is not part of user attributes and is handled
+ * separately
+ *
+ */
+class User
+{
+public:
+	/**
+	 * @brief User creat new user with username and displayname
+	 * @param username
+	 * @param displayname
+	 */
+	User(const string& username, const string& displayname);
+
+	/**
+	 * @brief User Creates new user from userdata
+	 * @param userdata json value with attributes
+	 *        must contain at least username
+	 */
+	User(const Json::Value& userdata);
+
+	/**
+	 * @brief GetUsername
+	 * @return objects username
+	 */
+	string GetUsername(void);
+
+	/**
+	 * @brief GetDisplayname
+	 * @return user displayname
+	 */
+	string GetDisplayname(void);
+
+	/**
+	 * @brief AddAttribute add attribute to user
+	 *
+	 * @param attr
+	 * @param value
+	 *
+	 * @note currently not synched to backend
+	 */
+	void AddAttribute(const string& attr, const string &value);
+
+	/**
+	 * @brief GetAttribute retrieve user attribute
+	 * @param attr
+	 * @return
+	 */
+	string GetAttribute(const string& attr);
+
+	/**
+	 * @brief ToJson serialize user to json object
+	 *
+	 * @return json object with one level key value attributes
+	 */
+	Json::Value ToJson(void);
+
+	virtual ~User();
+private:
+	string username;
+	string displayname;
+	map<string, string> attributes;
+};
+
+typedef shared_ptr<User> UserPtr;
 
 class UserManager : public BaseManager
 {
@@ -19,10 +102,16 @@ private:
 
 public:
 
-	static UserManager& Instance();
+	/**
+	 * @brief Instance, retrieve a new usermanager instance
+	 * @param authdb authdb to use or nullptr to use default
+	 * @return shared pointer to usagemanager or nullptr
+	 */
+	static UserManagerPtr Instance(OPI::SecopPtr authdb = nullptr);
 
 	/**
 	 * @brief AddUser add new user to system
+	 *			User is added to the system and basic email is setup
 	 * @param username
 	 * @param password
 	 * @param displayname Full name of new user
@@ -32,11 +121,117 @@ public:
 	bool AddUser(const string& username, const string& password, const string& displayname, bool isAdmin);
 
 	/**
+	 * @brief AddUser add new user to system
+	 * @param user userobject of user to add
+	 * @param password users password
+	 * @param isAdmin is this user an admin user?
+	 * @return true upon success
+	 */
+	bool AddUser(const UserPtr user, const string& password, bool isAdmin);
+
+	/**
+	 * @brief GetUser retrieve userinfo on user
+	 * @param username
+	 * @return shared pointer to user or nullptr
+	 */
+	UserPtr GetUser(const string& username);
+
+	/**
+	 * @brief UpdateUser Update a system user
+	 * @param user userobject to use for update, username must exist
+	 * @return true upon success
+	 */
+	bool UpdateUser(const UserPtr user);
+
+	/**
 	 * @brief DeleteUser delete user from system
 	 * @param user
 	 * @return true upon success
 	 */
 	bool DeleteUser(const string& user);
+
+	/**
+	 * @brief DeleteUser delete user from system
+	 * @param user
+	 * @return true upon success
+	 */
+	bool DeleteUser(const UserPtr user);
+
+	/**
+	 * @brief UpdateUserPassword
+	 * @param user User for which password should be updated
+	 * @param new_pass New password for user
+	 * @param old_pass Optional old password for user, if provided must
+	 *        match stored old password or operation will fail
+	 *
+	 * @return true upon success
+	 */
+	bool UpdateUserPassword(const string& user, const string& new_pass, const string& old_pass = "");
+
+	/**
+	 * @brief GetUsers get all users in system
+	 * @return list of shared pointers to user objects
+	 */
+	list<UserPtr> GetUsers(void);
+
+	/*
+	 *
+	 * Group management
+	 *
+	 */
+
+	/**
+	 * @brief GetGroups
+	 * @return list of group names on system
+	 */
+	list<string> GetGroups(void);
+
+	/**
+	 * @brief GetUserGroups
+	 * @param user
+	 * @return list of group names that user belongs to
+	 */
+	list<string> GetUserGroups(const string& user);
+
+
+	/**
+	 * @brief AddGroup create new group
+	 * @param groupname
+	 * @return true upon success
+	 */
+	bool AddGroup(const string& groupname);
+
+
+	/**
+	 * @brief DeleteGroup delete group
+	 * @param groupname
+	 * @return true upon success
+	 */
+	bool DeleteGroup(const string& groupname);
+
+	/**
+	 * @brief AddGroupMember add new member to group
+	 * @param group
+	 * @param member
+	 * @return true upon success
+	 */
+	bool AddGroupMember(const string& group, const string& member);
+
+	/**
+	 * @brief DeleteGroupMembar remove member from group
+	 * @param group
+	 * @param member
+	 * @return true upon success
+	 */
+	bool DeleteGroupMembar(const string& group, const string& member);
+
+	/**
+	 * @brief GetGroupMembers get all members of group
+	 * @param group
+	 * @return list of members
+	 */
+	list<string> GetGroupMembers(const string& group);
+
 
 	virtual ~UserManager();
 private:
