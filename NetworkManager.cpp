ecdb6be6e28d6e2976377db14f5158931ae7a291
Tor Krill <tor@openproducts.se>
2020-05-26 14:34:54 +0200

    Initial add of new NetworkManager class
    Signed-off-by: Tor Krill <tor@openproducts.se>

---
+++ NetworkManager.cpp
@@ -0,0 +1,80 @@
+#include "NetworkManager.h"
+
+#include <libopi/SysInfo.h>
+#include <libopi/NetworkConfig.h>
+
+#include <libutils/Logger.h>
+
+using namespace OPI;
+using namespace Utils;
+
+namespace KGP
+{
+
+NetworkManager::NetworkManager()
+{
+	logg << Logger::Debug << "NetworkManager starting up" << lend;
+
+	this->net = make_shared<OPI::NetUtils::NullConfig>();
+
+	// Check dependencies, a bit uggly concider refactor if more versions added
+	SysInfo::OSType os = sysinfo.OS();
+
+	if( os != SysInfo::OSType::OSDebian && os != SysInfo::OSType::OSRaspbian )
+	{
+		logg << Logger::Alert << "Network manager: unsupported platform: " << sysinfo.OSTypeText[os] << lend;
+		return;
+	}
+
+	string version = sysinfo.OSVersion();
+
+	if( version != "buster")
+	{
+		logg << Logger::Alert << "Unsupported raspbian/debian version: " << version << lend;
+		return;
+	}
+
+	if( os == SysInfo::OSType::OSDebian )
+	{
+		logg << Logger::Debug << "Running on valid debian system" << lend;
+		this->net = make_shared<OPI::NetUtils::DebianNetworkConfig>();
+	}
+
+	if( os == SysInfo::OSType::OSRaspbian )
+	{
+		logg << Logger::Debug << "Running on valid raspbian system" << lend;
+		this->net = make_shared<OPI::NetUtils::RaspbianNetworkConfig>();
+	}
+
+}
+
+NetworkManager &NetworkManager::Instance()
+{
+	static NetworkManager mgr;
+
+	return mgr;
+}
+
+Json::Value NetworkManager::GetConfiguration(const string &interface)
+{
+	return this->net->GetInterface(interface);
+}
+
+void NetworkManager::StaticConfiguration(const string &interface, const string &ip, const string &netmask, const string &gateway, const list<string> dns)
+{
+	this->net->SetStatic(interface,ip, netmask, gateway, dns);
+	this->net->WriteConfig();
+
+	OPI::NetUtils::RestartInterface(interface);
+}
+
+void NetworkManager::DynamicConfiguration(const string &interface)
+{
+	this->net->SetDHCP(interface);
+	this->net->WriteConfig();
+
+	OPI::NetUtils::RestartInterface(interface);
+}
+
+} // KGP
+
---
+++ NetworkManager.h
@@ -0,0 +1,73 @@
+#ifndef NETWORKMANAGER_H
+#define NETWORKMANAGER_H
+
+#include "BaseManager.h"
+
+#include <memory>
+#include <json/json.h>
+
+#include <libopi/NetworkConfig.h>
+
+namespace KGP
+{
+
+/**
+ * @brief The NetworkManager class
+ *
+ * The NetworkManager is a thin wrapper above the different
+ * Network config classes in libopi. Then manager makes sure the
+ * correct configurator is used on the platform
+ *
+ */
+
+class NetworkManager : public BaseManager
+{
+private:
+	NetworkManager();
+public:
+
+	static NetworkManager& Instance();
+
+	/**
+	 * @brief GetConfiguration fetches configuration for a network device
+	 *        on the systenm
+	 * @param interface, string naming device to fetch info from i.e. "eth0"
+	 *
+	 * @return A json object with current configuration
+	 *			"addressing" => "static" or "dhcp"
+	 *          "options" => "address" => "IP"
+	 *                       "netmask" => "netmask"
+	 *                       "gateway" => "gateway"
+	 *                       "dns" => ["list", "with" "dnss"]
+	 */
+	Json::Value GetConfiguration(const string& interface);
+
+	/**
+	 * @brief StaticConfiguration configure interface with static config
+	 * @param interface
+	 * @param ip
+	 * @param netmask
+	 * @param gateway
+	 * @param dns - list with dnss
+	 */
+	void StaticConfiguration(const string& interface,
+							 const string& ip,
+							 const string& netmask,
+							 const string& gateway,
+							 const list<string> dns );
+
+	/**
+	 * @brief DynamicConfiguration set dynamic configuration on interface
+	 * @param interface
+	 */
+	void DynamicConfiguration(const string& interface);
+
+	virtual ~NetworkManager() = default;
+private:
+	shared_ptr<OPI::NetUtils::NetworkConfig> net;
+};
+
+} //KGP
+
+
+#endif // NETWORKMANAGER_H
